0e7c150a11d8421bd5eb872717be44e863d8f136
RudeGalaxy1010 <Karev007@bk.ru>
2023-04-02 13:56:14 +0300

    Add waiting screen

---
+++ Assets/Source/Game/StartGamePause.cs
@@ -0,0 +1,27 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class StartGamePause : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject _cover;
+
+    private bool IsRoomFull => PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers;
+
+    private void Start()
+    {
+        CheckCover();
+    }
+
+    private void CheckCover()
+    {
+        if (PhotonNetwork.CurrentRoom != null)
+        {
+            _cover.SetActive(!IsRoomFull);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        CheckCover();
+    }
+}
